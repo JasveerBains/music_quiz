c6d8cc3b709ae1d5e7cf1978b97c67fecc056540
Jasveer Bains <jasbains2002@gmail.com>
2024-10-24 17:05:01 +0100

    change colours of album and artist cards to colours depending on length of title (randomish but constant)

--- frontend/app/components/AlbumResults.js
+++ frontend/app/components/AlbumResults.js
@@ -20,7 +20,18 @@ function toTitleCase(str) {
 
 function AlbumResults({query}) {
   const [albums, setAlbums] = useState([]);
-  const defaultImage = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/eb777e7a-7d3c-487e-865a-fc83920564a1/d7kpm65-437b2b46-06cd-4a86-9041-cc8c3737c6f0.jpg/v1/fill/w_800,h_800,q_75,strp/no_album_art__no_cover___placeholder_picture_by_cmdrobot_d7kpm65-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvZWI3NzdlN2EtN2QzYy00ODdlLTg2NWEtZmM4MzkyMDU2NGExXC9kN2twbTY1LTQzN2IyYjQ2LTA2Y2QtNGE4Ni05MDQxLWNjOGMzNzM3YzZmMC5qcGciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.8yjX5CrFjxVH06LB59TpJLu6doZb0wz8fGQq4tM64mg";
+  const colours = ['rgba(200,200,0,0.5)', 
+                    'rgba(0,200,200,0.5)', 
+                    'rgba(200,0,200,0.5)',
+                    'rgba(0,0,200,0.5)',
+                    'rgba(200,0,0,0.5)', 
+                    'rgba(0,200,0,0.5)', 
+                    'rgba(100,100,0,0.5)', 
+                    'rgba(0,100,100,0.5)', 
+                    'rgba(100,0,100,0.5)', 
+                    'rgba(0,0,100,0.5)', 
+                    'rgba(100,0,0,0.5)', 
+                    'rgba(0,100,0,0.5)'];
 
   useEffect(() => {
     const exampleFetch = () => {
@@ -35,27 +46,31 @@ function AlbumResults({query}) {
   }, [query])
 
   return (
-    <div className="App">
 
       <div className='albumCollection'>
         {
-          albums.map((a, i) => {
-            return (
-                <div key={i} className='albumCard'>
+          albums && albums.length > 0 ? (
+            albums.map((a, i) => {
+              const randomIndex = a.name.length*17 % colours.length;
+              const imageUrl = a.image[3]["#text"];
+              return (
+                <div key={i} className='albumCard' style={{backgroundColor: colours[randomIndex]}}>
                   <div className='albumImage'>
-                    <img src={a.image[3]["#text"] ? a.image[3]["#text"] : defaultImage} alt='album cover'></img>
+                    <img src={imageUrl ? imageUrl : null}></img>
                   </div>
 
                   <div className='albumText'>
                     <h3 >{toTitleCase(a.name)}</h3>
                     <h4 >{a.artist}</h4>
                   </div>
-              </div>
-            )
-          })
+                </div>
+              )
+            })
+          ) : (
+            <div>Could not find any albums.</div>
+          )
         }
       </div>
-    </div>
   );
 }
 
--- frontend/app/components/ArtistResults.js
+++ frontend/app/components/ArtistResults.js
@@ -19,8 +19,21 @@ function toTitleCase(str) {
 
 
 function ArtistResults({query}) {
-  const [Artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState([]);
   const defaultImage = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/eb777e7a-7d3c-487e-865a-fc83920564a1/d7kpm65-437b2b46-06cd-4a86-9041-cc8c3737c6f0.jpg/v1/fill/w_800,h_800,q_75,strp/no_Artist_art__no_cover___placeholder_picture_by_cmdrobot_d7kpm65-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvZWI3NzdlN2EtN2QzYy00ODdlLTg2NWEtZmM4MzkyMDU2NGExXC9kN2twbTY1LTQzN2IyYjQ2LTA2Y2QtNGE4Ni05MDQxLWNjOGMzNzM3YzZmMC5qcGciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.8yjX5CrFjxVH06LB59TpJLu6doZb0wz8fGQq4tM64mg";
+  const colours = ['rgba(200,200,0,0.5)', 
+    'rgba(0,200,200,0.5)', 
+    'rgba(200,0,200,0.5)',
+    'rgba(0,0,200,0.5)',
+    'rgba(200,0,0,0.5)', 
+    'rgba(0,200,0,0.5)', 
+    'rgba(100,100,0,0.5)', 
+    'rgba(0,100,100,0.5)', 
+    'rgba(100,0,100,0.5)', 
+    'rgba(0,0,100,0.5)', 
+    'rgba(100,0,0,0.5)', 
+    'rgba(0,100,0,0.5)'];
+
 
   useEffect(() => {
     const exampleFetch = () => {
@@ -39,11 +52,12 @@ function ArtistResults({query}) {
 
         <div className='Collection'>
             {
-            Artists.map((a, i) => {
-                return (
-                    <div key={i}>
-                        { a.listeners > 100000 ? (
-                            <div className='Card'>
+            artists && artists.length > 0 ? (
+                artists.map((a, i) => {
+                    const randomIndex = a.name.length*17 % colours.length;
+                    return (
+                        <div key={i}>
+                            <div className='Card' style={{backgroundColor: colours[randomIndex]}}>
                                 <div className='Image'>
                                     <img src={a.image[3]["#text"] ? a.image[3]["#text"] : defaultImage} alt='Artist cover'></img>
                                 </div>
@@ -52,11 +66,12 @@ function ArtistResults({query}) {
                                     <h3 >{a.name}</h3>
                                 </div>
                             </div>
-
-                        ) : <div></div> }
-                    </div>
-                )
-            })
+                        </div>
+                    )
+                })
+            ) : (
+                <div>Could not find any artists.</div>
+            )
             }
         </div>
     </div>
